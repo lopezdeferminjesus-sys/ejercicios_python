d310a4fe1f5c48449349ab881ab51341c505c420
lopezdefermin-sys <lopezdeferminjesus@gmail.com>
2026-01-22 02:56:40 -0600

    mi primer commit

---
+++ ejercicio_01.py
@@ -0,0 +1,13 @@
+# Escribir un programa que pregunte al usuario su nombre, y luego lo salude.
+
+#funcion input() Resibe un string y regresa un string 
+
+name = input("Escribe tu nombre: ")
+
+print("Hola", name, "XD") # print recibe uno o mas valores (,)
+
+print("Hola" + name) # Recibe un estring concatenado
+
+print("Hola" + name)
+
+print("Hola" + name + " XD")
---
+++ strings.py
@@ -0,0 +1,28 @@
+# string con Oythin
+
+my_string = "Este es un String"
+print(my_string)
+
+other_string = 'Este es otro String'
+print(other_string)
+my_self = "I'm Jesus"
+print(my_self)
+other_string = 'Este es otro String'
+print(other_string)
+my_self = "I'm Jesus"
+print(my_self)
+some = 'Hello is "Hola"'
+print(some)
+name = "Jesus"
+
+age = 18
+
+city = 'Los Ameles'
+
+message_1 = "Yo soy" + name + ', vivo en ' + city + " y tengo " + str (age) + " años "
+
+print(message_1)
+
+message_2 = f"Yo soy { name }, vivo en { city } y tengo { age } años "
+
+print(message_2)
