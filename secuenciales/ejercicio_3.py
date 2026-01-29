63d355a917a334a56a33173922176b401ec49cc4
lopezdeferminjesus-sys <lopezdeferminjesus@gmail.com>
2026-01-28 20:57:45 -0600

    Delete secuenciales directory

--- secuenciales/ejercicio_3.py
+++
@@ -1,8 +0,0 @@
-
-
-cateto_1 = int(input('ingresa el cateto 1: '))
-cateto_2 = int(input('ingresa el cateto 2: '))
-
-hipotenusa = (cateto_1 ** 2 + cateto_2 **2) ** (1 / 2)
-
-print('La hipotenusa es:', hipotenusa)
