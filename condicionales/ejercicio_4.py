7966bb8106b6481c8378fff94457e3e8c0d27ef4
lopezdeferminjesus-sys <lopezdeferminjesus@gmail.com>
2026-02-12 19:21:08 -0600

    Delete condicionales directory

--- condicionales/ejercicio_4.py
+++
@@ -1,8 +0,0 @@
-
-numero = int(input("Dime un número de dos cifras: "))
-
-decenas = numero // 10
-unidades = numero % 10
-
-print("Primera cifra (decenas):", decenas)
-print("Segunda cifra (unidades):", unidades)
